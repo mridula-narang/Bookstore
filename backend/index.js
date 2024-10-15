268e3bf4d4b98f80ef29759a2bc307bcffd2044e
mridula-narang <narangmg@gmail.com>
2024-10-15 21:13:29 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,22 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { Book } from '../backend/models/bookModel.js';
 import bookRoute from '../backend/routes/bookRoute.js';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 
+app.use(cors({
+    origin:'http://localhost:3000',
+    methods:['GET','POST','PUT','DELETE'],
+    allowedHeaders:['Content-type']
+}));
+
 mongoose.connect('mongodb+srv://mridulagn123:Dauntless1234@cluster0.1anp1bv.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0')
 .then(()=>{
     console.log('Connected to MongoDB');
-    app.listen(3000,()=>{
-        console.log("App is listening at port: ", 3000);
+    app.listen(5555,()=>{
+        console.log("App is listening at port: ", 5555);
     });
 })
 .catch(err => console.error('Could not connect to MongoDB', err));
