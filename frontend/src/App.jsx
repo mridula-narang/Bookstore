8bf9cfdc21116702d6dc8d5f87f1f66742b9b78c
mridula-narang <narangmg@gmail.com>
2024-10-15 21:35:20 +0530

    created react components1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBooks from './pages/CreateBooks';
+import EditBook from './pages/EditBook';
+import ShowBook from './pages/ShowBook';
+import DeleteBook from './pages/DeleteBook';
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>
-      hello world
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/book/create' element={<CreateBooks/>} />
+      <Route path='/book/details/:id' element={<ShowBook/>} />
+      <Route path='/book/edit/:id' element={<EditBook/>} />
+      <Route path='/book/delete/:id' element={<DeleteBook/>} />
+    </Routes>
   );
 }
 
