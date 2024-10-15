2818a080fef28b117c10c105644fe5bcf51ef540
mridula-narang <narangmg@gmail.com>
2024-10-15 20:02:57 +0530

    created models

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
 const app = express();
 
-app.listen(3000,()=>{
-    console.log("App is listening at port: ",3000 );
+mongoose.connect('mongodb+srv://mridulagn123:Dauntless1234@cluster0.1anp1bv.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0')
+.then(()=>{
+    console.log('Connected to MongoDB')
+    app.listen(3000,()=>{
+        console.log("App is listening at port: ",3000 );
+    });
+})
+.catch(err=>console.error('Could not connect to MongoDB',err));
+
+app.get('/',(req,res)=>{
+    res.send("Hello World");
 });
+
---
+++ backend/models/bookModel.js
@@ -0,0 +1,14 @@
+import mongoose, { Mongoose } from "mongoose";
+const bookSchema = new Mongoose.Schema(
+    {
+        title: { type: String, required: true },
+        author: { type: String, required: true },
+        publishYear: { type: String, required: true },
+    },
+    {
+        timestamps: true,
+    }
+
+);
+
+export const Book = mongoose.model('Book', bookSchema);
